2b7d57de1a6b127c9a66850b5a4f2efaec468782
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-13 16:10:08 +0530

    add locale config be component

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -1,11 +1,23 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 import { useAppStore } from '@/store'
+import { dateZhCN, zhCN, enUS, dateEnUS } from 'naive-ui'
+import type { NLocale, NDateLocale } from 'naive-ui'
 
 const appStore = useAppStore()
+
+const localeMap: Record<string, { locale: NLocale; dateLocale: NDateLocale }> = {
+  zh_CN: { locale: zhCN, dateLocale: dateZhCN },
+  en: { locale: enUS, dateLocale: dateEnUS }
+}
 </script>
 
 <template>
-  <router-view v-slot="{ Component, route }">
-    <component :is="Component" v-if="appStore.reloadFlag" :key="route.path" />
-  </router-view>
+  <n-config-provider
+    :locale="localeMap[$i18n.locale].locale"
+    :date-locale="localeMap[$i18n.locale].dateLocale"
+  >
+    <router-view v-slot="{ Component, route }">
+      <component :is="Component" v-if="appStore.reloadFlag" :key="route.path" />
+    </router-view>
+  </n-config-provider>
 </template>
