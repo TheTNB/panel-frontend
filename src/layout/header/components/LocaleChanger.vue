d25260ee523db877ffaa838cc7db401d934e86ce
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-13 21:50:31 +0530

    style locale changer

--- src/layout/header/components/LocaleChanger.vue
+++ src/layout/header/components/LocaleChanger.vue
@@ -1,11 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { SelectOption, SelectGroupOption } from 'naive-ui'
+import { useI18n } from 'vue-i18n'
 
+const { availableLocales } = useI18n()
+
+const options: Array<SelectOption | SelectGroupOption> = availableLocales.map((locale) => ({
+  label: locale,
+  value: locale
+}))
+</script>
 <template>
   <div class="locale-changer">
-    <select v-model="$i18n.locale">
-      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
-        {{ locale }}
-      </option>
-    </select>
+    <n-space vertical mr-20>
+      <n-select v-model:value="$i18n.locale" :options="options" />
+    </n-space>
   </div>
 </template>
