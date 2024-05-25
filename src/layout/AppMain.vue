4832063093927001cbd0f809a0e9d11ee6d239ea
耗子 <i@haozi.net>
2024-05-26 02:17:37 +0800

    revert: add locale config to fix page scrolling

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -1,14 +1,11 @@
-<script setup lang="ts">
-import { useAppStore, useThemeStore } from '@/store'
+<script lang="ts" setup>
+import { useAppStore } from '@/store'
 
 const appStore = useAppStore()
-const themeStore = useThemeStore()
 </script>
 
 <template>
-  <n-config-provider :locale="themeStore.naiveLocale" :date-locale="themeStore.naiveDateLocale">
-    <router-view v-slot="{ Component, route }">
-      <component :is="Component" v-if="appStore.reloadFlag" :key="route.path" />
-    </router-view>
-  </n-config-provider>
+  <router-view v-slot="{ Component, route }">
+    <component :is="Component" v-if="appStore.reloadFlag" :key="route.path" />
+  </router-view>
 </template>
